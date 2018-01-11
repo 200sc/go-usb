62a99092e606f1bc88fda62d0244a4f3e1f60c31
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-01-11 09:15:27 -0600

    Ported over libusb.h

--- libusb.go
+++ libusb.go
@@ -1,5 +1,9 @@
 package usb
 
+import (
+	"encoding/binary"
+)
+
 /*
  * Public libusb header file
  * Copyright © 2001 Johannes Erdfelt <johannes@erdfelt.com>
@@ -963,14 +967,14 @@ type libusb_control_setup struct {
 	 * \ref libusb_request_type. Bit 7 determines data transfer direction, see
 	 * \ref libusb_endpoint_direction.
 	 */
-	 bmRequestType uint8
+	bmRequestType uint8
 
 	/** Request. If the type bits of bmRequestType are equal to
 	 * \ref libusb_request_type::LIBUSB_REQUEST_TYPE_STANDARD
 	 * "LIBUSB_REQUEST_TYPE_STANDARD" then this field refers to
 	 * \ref libusb_standard_request. For other cases, use of this field is
 	 * application-specific. */
-	 bRequest uint8
+	bRequest uint8
 
 	/** Value. Varies according to request */
 	wValue uint16
@@ -1125,7 +1129,19 @@ const LIBUSB_API_VERSION = 0x01000105
   * \returns the value in host-endian byte order
   */
 var libusb_le16_to_cpu = libusb_cpu_to_le16
- 
+
+/**
+ * \ingroup libusb_misc
+ * Convert a 16-bit value from host-endian to little-endian format. On
+ * little endian systems, this function does nothing. On big endian systems,
+ * the bytes are swapped.
+ * \param x the host-endian value to convert
+ * \returns the value in little-endian byte order
+ */
+// Go credit: This is swapUint16 from github.com/tsuna/endian
+func libusb_cpu_to_le16(x uint16) uint16 {
+	return (n&0x00FF)<<8 | (n&0xFF00)>>8
+}
  
  /* Descriptor sizes per descriptor type */
  const LIBUSB_DT_DEVICE_SIZE =			18
@@ -1260,4 +1276,325 @@ func libusb_get_iso_packet_buffer_simple(transfer *libusb_transfer, packet int) 
 
 	// GO this needs to be fixed, buffer should probably not be a []uint8
 	return transfer.buffer + ((int) transfer.iso_packet_desc[0].length * packet);
+}
+
+/** \ingroup libusb_asyncio
+ * Get the data section of a control transfer. This convenience function is here
+ * to remind you that the data does not start until 8 bytes into the actual
+ * buffer, as the setup packet comes first.
+ *
+ * Calling this function only makes sense from a transfer callback function,
+ * or situations where you have already allocated a suitably sized buffer at
+ * transfer->buffer.
+ *
+ * \param transfer a transfer
+ * \returns pointer to the first byte of the data section
+ */
+func libusb_control_transfer_get_data(transfer *libusb_transfer) []uint8 {
+	return transfer.buffer[LIBUSB_CONTROL_SETUP_SIZE:]
+}
+
+/** \ingroup libusb_asyncio
+ * Get the control setup packet of a control transfer. This convenience
+ * function is here to remind you that the control setup occupies the first
+ * 8 bytes of the transfer data buffer.
+ *
+ * Calling this function only makes sense from a transfer callback function,
+ * or situations where you have already allocated a suitably sized buffer at
+ * transfer->buffer.
+ *
+ * \param transfer a transfer
+ * \returns a casted pointer to the start of the transfer data buffer
+ */
+// go api change: this returns a byte slice so it preserves
+// its intention of having manipulatable shared data.
+// consider in the future having this return a struct built of pointers
+// also consider: not using an arbitrary buffer but having these 
+// data and setup sections be their own fields
+func libusb_control_transfer_get_setup(transfer *libusb_transfer) []uint8 {
+	return transfer.buffer[:LIBUSB_CONTROL_SETUP_SIZE]
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the setup packet (first 8 bytes of the data
+ * buffer) for a control transfer. The wIndex, wValue and wLength values should
+ * be given in host-endian byte order.
+ *
+ * \param buffer buffer to output the setup packet into
+ * This pointer must be aligned to at least 2 bytes boundary.
+ * \param bmRequestType see the
+ * \ref libusb_control_setup::bmRequestType "bmRequestType" field of
+ * \ref libusb_control_setup
+ * \param bRequest see the
+ * \ref libusb_control_setup::bRequest "bRequest" field of
+ * \ref libusb_control_setup
+ * \param wValue see the
+ * \ref libusb_control_setup::wValue "wValue" field of
+ * \ref libusb_control_setup
+ * \param wIndex see the
+ * \ref libusb_control_setup::wIndex "wIndex" field of
+ * \ref libusb_control_setup
+ * \param wLength see the
+ * \ref libusb_control_setup::wLength "wLength" field of
+ * \ref libusb_control_setup
+ */
+func libusb_fill_control_setup(buffer []uint8, bmRequestType, bRequest uint8, wValue, wIndex, wLength uint16) {
+	buffer[0] = bmRequestType
+	buffer[1] = bRequest
+	wValue = libusb_cpu_to_le16(wValue)
+	buffer[2] = uint8(wValue >> 8)
+	buffer[3] = uint8(wValue)
+	wIndex = libusb_cpu_to_le16(wIndex)
+	buffer[4] = uint8(wIndex >> 8)
+	buffer[5] = uint8(wIndex)
+	wLength = libusb_cpu_to_le16(wLength)
+	buffer[6] = uint8(wLength >> 8)
+	buffer[7] = uint8(wLength)
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the required \ref libusb_transfer fields
+ * for a control transfer.
+ *
+ * If you pass a transfer buffer to this function, the first 8 bytes will
+ * be interpreted as a control setup packet, and the wLength field will be
+ * used to automatically populate the \ref libusb_transfer::length "length"
+ * field of the transfer. Therefore the recommended approach is:
+ * -# Allocate a suitably sized data buffer (including space for control setup)
+ * -# Call libusb_fill_control_setup()
+ * -# If this is a host-to-device transfer with a data stage, put the data
+ *    in place after the setup packet
+ * -# Call this function
+ * -# Call libusb_submit_transfer()
+ *
+ * It is also legal to pass a NULL buffer to this function, in which case this
+ * function will not attempt to populate the length field. Remember that you
+ * must then populate the buffer and length fields later.
+ *
+ * \param transfer the transfer to populate
+ * \param dev_handle handle of the device that will handle the transfer
+ * \param buffer data buffer. If provided, this function will interpret the
+ * first 8 bytes as a setup packet and infer the transfer length from that.
+ * This pointer must be aligned to at least 2 bytes boundary.
+ * \param callback callback function to be invoked on transfer completion
+ * \param user_data user data to pass to callback function
+ * \param timeout timeout for the transfer in milliseconds
+ */
+func libusb_fill_control_transfer(transfer *libusb_transfer,
+	dev_handle *libusb_device_handle,
+	buffer []uint8, callback libusb_transfer_cb_fn, user_data interface{},
+	timeout uint) {
+
+	transfer.dev_handle = dev_handle
+	transfer.endpoint = 0
+	transfer.type = LIBUSB_TRANSFER_TYPE_CONTROL
+	transfer.timeout = timeout
+	transfer.buffer = buffer
+	if len(buffer) > 8 {
+		setupLen := binary.LittleEndian.Uint16([]byte{buffer[6], buffer[7]})
+		transfer.length = int32(LIBUSB_CONTROL_SETUP_SIZE + libusb_le16_to_cpu(setup.wLength))
+	}
+	transfer.user_data = user_data
+	transfer.callback = callback
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the required \ref libusb_transfer fields
+ * for a bulk transfer.
+ *
+ * \param transfer the transfer to populate
+ * \param dev_handle handle of the device that will handle the transfer
+ * \param endpoint address of the endpoint where this transfer will be sent
+ * \param buffer data buffer
+ * \param length length of data buffer
+ * \param callback callback function to be invoked on transfer completion
+ * \param user_data user data to pass to callback function
+ * \param timeout timeout for the transfer in milliseconds
+ */
+func libusb_fill_bulk_transfer(transfer *libusb_transfer,
+	dev_handle *libusb_device_handle, endpoint uint8,
+	buffer []uint8, length int, callback libusb_transfer_cb_fn,
+	user_data interface{}, timeout uint) {
+
+	transfer.dev_handle = dev_handle
+	transfer.endpoint = endpoint
+	transfer.type = LIBUSB_TRANSFER_TYPE_BULK
+	transfer.timeout = timeout
+	transfer.buffer = buffer
+	transfer.length = length
+	transfer.user_data = user_data
+	transfer.callback = callback
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the required \ref libusb_transfer fields
+ * for a bulk transfer using bulk streams.
+ *
+ * Since version 1.0.19, \ref LIBUSB_API_VERSION >= 0x01000103
+ *
+ * \param transfer the transfer to populate
+ * \param dev_handle handle of the device that will handle the transfer
+ * \param endpoint address of the endpoint where this transfer will be sent
+ * \param stream_id bulk stream id for this transfer
+ * \param buffer data buffer
+ * \param length length of data buffer
+ * \param callback callback function to be invoked on transfer completion
+ * \param user_data user data to pass to callback function
+ * \param timeout timeout for the transfer in milliseconds
+ */
+func libusb_fill_bulk_stream_transfer(transfer *libusb_transfer, 
+	dev_handle *libusb_device_handle,
+	endpoint uint8, stream_id uint32,
+	buffer []uint8, length int, callback libusb_transfer_cb_fn,
+	user_data interface{}, timeout uint) {
+
+	libusb_fill_bulk_transfer(transfer, dev_handle, endpoint, buffer,
+				  length, callback, user_data, timeout)
+	transfer.type = LIBUSB_TRANSFER_TYPE_BULK_STREAM
+	libusb_transfer_set_stream_id(transfer, stream_id)
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the required \ref libusb_transfer fields
+ * for an interrupt transfer.
+ *
+ * \param transfer the transfer to populate
+ * \param dev_handle handle of the device that will handle the transfer
+ * \param endpoint address of the endpoint where this transfer will be sent
+ * \param buffer data buffer
+ * \param length length of data buffer
+ * \param callback callback function to be invoked on transfer completion
+ * \param user_data user data to pass to callback function
+ * \param timeout timeout for the transfer in milliseconds
+ */
+func libusb_fill_interrupt_transfer(transfer *libusb_transfer, 
+	dev_handle *libusb_device_handle,
+	endpoint uint8, buffer []uint8, length int,
+	callback libusb_transfer_cb_fn, user_data interface{}, timeout uint) {
+
+	transfer.dev_handle = dev_handle
+	transfer.endpoint = endpoint
+	transfer.type = LIBUSB_TRANSFER_TYPE_INTERRUPT
+	transfer.timeout = timeout
+	transfer.buffer = buffer
+	transfer.length = length
+	transfer.user_data = user_data
+	transfer.callback = callback
+}
+
+/** \ingroup libusb_asyncio
+ * Helper function to populate the required \ref libusb_transfer fields
+ * for an isochronous transfer.
+ *
+ * \param transfer the transfer to populate
+ * \param dev_handle handle of the device that will handle the transfer
+ * \param endpoint address of the endpoint where this transfer will be sent
+ * \param buffer data buffer
+ * \param length length of data buffer
+ * \param num_iso_packets the number of isochronous packets
+ * \param callback callback function to be invoked on transfer completion
+ * \param user_data user data to pass to callback function
+ * \param timeout timeout for the transfer in milliseconds
+ */
+func libusb_fill_iso_transfer(transfer *libusb_transfer,
+	dev_handle *libusb_device_handle, endpoint uint8,
+	buffer []uint8, length, num_iso_packets int,
+	callback libusb_transfer_cb_fn, user_data interface{}, timeout uint) {
+
+	transfer.dev_handle = dev_handle
+	transfer.endpoint = endpoint
+	transfer.type = LIBUSB_TRANSFER_TYPE_ISOCHRONOUS
+	transfer.timeout = timeout
+	transfer.buffer = buffer
+	transfer.length = length
+	transfer.num_iso_packets = num_iso_packets
+	transfer.user_data = user_data
+	transfer.callback = callback
+}
+
+/** \ingroup libusb_asyncio
+ * Convenience function to set the length of all packets in an isochronous
+ * transfer, based on the num_iso_packets field in the transfer structure.
+ *
+ * \param transfer a transfer
+ * \param length the length to set in each isochronous packet descriptor
+ * \see libusb_get_max_packet_size()
+ */
+func libusb_set_iso_packet_lengths(transfer *libusb_transfer, length uint) {
+	for i := 0; i < transfer.num_iso_packets; i++ {
+		transfer.iso_packet_desc[i].length = length
+	}
+}
+
+/** \ingroup libusb_asyncio
+ * Convenience function to locate the position of an isochronous packet
+ * within the buffer of an isochronous transfer.
+ *
+ * This is a thorough function which loops through all preceding packets,
+ * accumulating their lengths to find the position of the specified packet.
+ * Typically you will assign equal lengths to each packet in the transfer,
+ * and hence the above method is sub-optimal. You may wish to use
+ * libusb_get_iso_packet_buffer_simple() instead.
+ *
+ * \param transfer a transfer
+ * \param packet the packet to return the address of
+ * \returns the base address of the packet buffer inside the transfer buffer,
+ * or NULL if the packet does not exist.
+ * \see libusb_get_iso_packet_buffer_simple()
+ */
+func libusb_get_iso_packet_buffer(transfer *libusb_transfer, packet int32) ([]uint8, error) {
+	// Go api difference: packet is an int32, not a uint.
+
+	if (packet >= transfer.num_iso_packets) {
+		return []uint8{}, errors.New("packet exceeds transfer's iso packet count")
+	}
+
+	offset := 0
+	for i := 0; i < packet; i++ {
+		offset += transfer.iso_packet_desc[i].length
+	}
+
+	return transfer.buffer[offset:], nil
+}
+
+/** \ingroup libusb_desc
+ * Retrieve a descriptor from the default control pipe.
+ * This is a convenience function which formulates the appropriate control
+ * message to retrieve the descriptor.
+ *
+ * \param dev_handle a device handle
+ * \param desc_type the descriptor type, see \ref libusb_descriptor_type
+ * \param desc_index the index of the descriptor to retrieve
+ * \param data output buffer for descriptor
+ * \param length size of data buffer
+ * \returns number of bytes returned in data, or LIBUSB_ERROR code on failure
+ */
+func libusb_get_descriptor(dev_handle *libusb_device_handle, desc_type, 
+	desc_index uint8, data []uint8, length int) int {
+
+	return libusb_control_transfer(dev_handle, LIBUSB_ENDPOINT_IN,
+		LIBUSB_REQUEST_GET_DESCRIPTOR, uint16(((desc_type << 8) | desc_index)),
+		0, data, uint16(length), 1000)
+}
+
+/** \ingroup libusb_desc
+ * Retrieve a descriptor from a device.
+ * This is a convenience function which formulates the appropriate control
+ * message to retrieve the descriptor. The string returned is Unicode, as
+ * detailed in the USB specifications.
+ *
+ * \param dev_handle a device handle
+ * \param desc_index the index of the descriptor to retrieve
+ * \param langid the language ID for the string descriptor
+ * \param data output buffer for descriptor
+ * \param length size of data buffer
+ * \returns number of bytes returned in data, or LIBUSB_ERROR code on failure
+ * \see libusb_get_string_descriptor_ascii()
+ */
+func libusb_get_string_descriptor(dev_handle *libusb_device_handle, desc_index uint8,
+	langid uint16, data []uint8, length int) int {
+
+	return libusb_control_transfer(dev_handle, LIBUSB_ENDPOINT_IN,
+		LIBUSB_REQUEST_GET_DESCRIPTOR, uint16(((LIBUSB_DT_STRING << 8) | desc_index)),
+		langid, data, uint16(length), 1000)
 }
