9b96ef617c8f81e598ee4e8915bba1b24c751a66
Patrick Stephen <patrick.d.stephen@gmail.com>
2018-01-23 20:38:58 -0600

    Confident everything in windows_common.h is in the stdlib

--- os/windows_common.go
+++ os/windows_common.go
@@ -25,3 +25,7 @@ package os
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
+const  (
+	ERR_BUFFER_SIZE	256
+)
